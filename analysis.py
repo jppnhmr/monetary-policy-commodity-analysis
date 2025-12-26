da03ac2a5d6ebb812834de001b2710b271c5bfaa
jppnhmr <jppnhmr@gmail.com>
2025-12-26 12:20:39 +0000

    Fixed x and y axis labels not displaying

--- analysis.py
+++ analysis.py
@@ -35,8 +35,8 @@ def plot_data(data: List[tuple], title, y_axis, y_unit):
     plt.xticks(rotation=45)
     plt.tight_layout()
 
-    plt.ylabel = y_axis
-    plt.xlabel = 'date'
+    plt.ylabel(y_axis)
+    plt.xlabel('date')
     plt.show()
 
 def cli_plot_data():
